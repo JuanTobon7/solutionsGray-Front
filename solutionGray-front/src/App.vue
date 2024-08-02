577c6a2e27db4006bfb223c881f0240f14aa7d8c
JuanTobon7 <jctobon11.2@gmail.com>
2024-08-01 22:44:29 -0500

    responsividad y get inforamcion

--- solutionGray-front/src/App.vue
+++ solutionGray-front/src/App.vue
@@ -1,25 +1,18 @@
 <template>
-  <div class="flex">
-    <div class="w-[20%]">
-      <MenuUser />
-    </div>
-    <div class="flex flex-col">
-      <NavBar />
-      <main class="mt-[20%]">
-        <RouterView />
-      </main>
-    </div>
-  </div>
+  <NavBar/>
+  <main>
+    <RouterView/>
+  </main>
 </template>
 
 <script>
-import MenuUser from './components/MenuUser.vue';
 import NavBar from './components/NavBar.vue';
+import { RouterView } from 'vue-router';
 
 export default {
   components: {
-    MenuUser,
-    NavBar
+    NavBar,
+    RouterView
   }
 };
 </script>
--- solutionGray-front/src/components/NavBar.vue
+++ solutionGray-front/src/components/NavBar.vue
@@ -1,24 +1,28 @@
 <template>
-    <section class="flex py-5 bg-primary-500 fixed w-[80%]">
+  <section class="w-full">
+    <div class="fixed top-0 right-0 md:w-full  lg:w-[calc(100%-15rem)] bg-primary-500 py-5">
       <nav class="flex justify-between items-center container px-8">
-            <span class="material-icons text-white">menu</span>
-            <div class=" flex right-0 text-primary-50 gap-4 items-center">
-                <img class="h-11 w-11" src="../assets//solutionGrayLOGO-removebg.png">
-                <p class="text-xl">Juan Carlos Tobon</p>
-            </div>
+        <span class="material-icons text-white">menu</span>
+        <div class="flex right-0 text-primary-50 gap-4 items-center">
+          <img class="h-11 w-11" src="../assets/solutionGrayLOGO-removebg.png">
+          <p class="text-xl">Juan Carlos Tobon</p>
+        </div>
       </nav>
-    </section>
-  </template>
-  
-  <script>
-  export default {
-    name: 'NavBar'
-  };
-  </script>
-  
-  <style scoped>
-  /* Puedes añadir estilos personalizados aquí */
+    </div>
+    <MenuUser/>
+  </section>
+</template>
 
+<script>
+import MenuUser from './MenuUser.vue';
+
+export default {
+  name: 'NavBar',
+  components: {
+    MenuUser
+  }
+};
+</script>
+
+<style scoped>
 </style>
-
-  
