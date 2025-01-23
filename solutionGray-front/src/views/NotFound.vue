d54f25c70c22ea2608751a93c8f98c06618e84f3
JuanTobon7 <jctobon@unillanos.edu.co>
2025-01-23 16:43:38 -0500

    feat: Update NotFound view styling and enhance navigation logic to exclude NotFound route from navbar

--- solutionGray-front/src/views/NotFound.vue
+++ solutionGray-front/src/views/NotFound.vue
@@ -1,15 +1,17 @@
 <template>
-    <div class="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+    <div class="flex flex-col items-center justify-center min-h-screen bg-primary-50">
       <!-- Título -->
-      <h1 class="text-8xl font-extrabold text-primary">404</h1>
+      <h1 class="text-8xl font-extrabold text-primary-900">404</h1>
       <!-- Subtítulo -->
       <p class="mt-4 text-xl text-gray-600">Lo sentimos, la página que buscas no existe 😢</p>
       <!-- Imagen opcional -->
-      <img src="/images/404-illustration.svg" alt="Página no encontrada" class="w-64 h-64 mt-8" />
+       <i class="material-symbols-outlined text-9xl text-primary-900">
+        sentiment_dissatisfied
+       </i>
       <!-- Botón para volver al inicio -->
       <router-link
         to="/"
-        class="mt-6 px-6 py-3 text-white bg-secondary rounded-lg shadow-md hover:bg-secondary-dark hover:shadow-lg transition duration-300"
+        class="mt-6 px-6 py-3  bg-primary-500 rounded-lg shadow-md hover:bg-primary-600 hover:shadow-lg transition duration-300"
       >
         Volver al inicio
       </router-link>
